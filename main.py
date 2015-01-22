2739aaef0e2e1751e86d91d0953143dbfc57c671
Dmitriy Demidov <dmitdemidov@gmail.com>
2015-01-23 01:15:48 +0300

    done

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ if __name__ == '__main__':
     informer = partition.WindowsInformer()
 
     if len(sys.argv) == 1:
-        print(informer.disk_list())
+        print("Disk Size")
+        for d in informer.disk_list():
+            print("%s\t%s" % (d[0], d[1]))
     else:
-        print(informer.partition_list(sys.argv[1]))
+        print("Partition Size")
+        for p in informer.partition_list(sys.argv[1]):
+            print("%s\t%s" % (p[0], p[1]))
 
 
 
